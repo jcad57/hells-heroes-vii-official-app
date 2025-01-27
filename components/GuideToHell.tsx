8e2c00ab7ad800ce83779b0c84f6b7503bd71433
Jon <jcadeichmann@gmail.com>
2025-01-26 18:11:47 -0600

    Connect guide-to-hell data from firebase to MapView component (still needs work)

--- components/GuideToHell.tsx
+++ components/GuideToHell.tsx
@@ -1,78 +1,104 @@
 import { StyleSheet, View } from "react-native";
-import { useRef, useState } from "react";
-import MapView, { Marker, Region } from "react-native-maps";
+import { useEffect, useRef, useState } from "react";
+import { db } from "@/firebase/firebase";
+import { collection, getDocs, query } from "firebase/firestore";
+import MapView, { LatLng, Marker, Region } from "react-native-maps";
 
 import PageHeading from "./PageHeading";
 
 const TEMPMARKERS = [
-  {
-    id: 2,
-    title: "White Oak Music Hall",
-    description: "A pretty cool music venue with a hot Senior Marketing Director",
-    location: { latitude: 29.786213265882278, longitude: -95.36701106479163 },
-  },
-  {
-    id: 3,
-    title: "Chick-fil-A",
-    description: "Gross AF",
-    location: { latitude: 29.775518150360003, longitude: -95.38087943169278 },
-  },
+    {
+        id: "1",
+        name: "Hell's Heroes HQ",
+        description: "The home of Hell's Heroes",
+        location: { latitude: 29.786213265882278, longitude: -95.36701106479163 },
+    },
 ];
 
-interface InitialRegion {
-  latitude: Number;
-  longitude: Number;
-  latitudeDelta: Number;
-  longitudeDelta: Number;
-}
-
 const initialRegion: Region = {
-  latitude: 29.786213265882278,
-  longitude: -95.36701106479163,
-  latitudeDelta: 0.0922,
-  longitudeDelta: 0.0421,
+    latitude: 29.786213265882278,
+    longitude: -95.36701106479163,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
 };
 
 const newRegion = {
-  latitude: 27.786213265882278,
-  longitude: -94.36701106479163,
+    latitude: 27.786213265882278,
+    longitude: -94.36701106479163,
 };
 
-export default function GuideToHell() {
-  const [region, setRegion] = useState(initialRegion);
-  const mapRef = useRef<MapView>(null);
+interface BusinessListData {
+    id: string;
+    name: string;
+    description: string;
+    location: LatLng;
+}
 
-  function handleMarkerPress(marker) {
-    setRegion(marker.location);
-  }
-  return (
-    <View style={styles.container}>
-      <PageHeading text="Guide to Hell" />
-      <MapView ref={mapRef} style={styles.map} initialRegion={initialRegion} region={region} mapType="mutedStandard">
-        {TEMPMARKERS.map((marker) => {
-          return (
-            <Marker
-              key={marker.id}
-              coordinate={marker.location}
-              title={marker.title}
-              description={marker.description}
-              onSelect={() => handleMarkerPress(marker)}
-            />
-          );
-        })}
-      </MapView>
-    </View>
-  );
+export default function GuideToHell() {
+    const [region, setRegion] = useState(initialRegion);
+    const [isLoading, setIsLoading] = useState(false);
+    const [businessList, setBusinessList] = useState(TEMPMARKERS);
+
+    const mapRef = useRef<MapView>(null);
+
+    useEffect(() => {
+        const fetchBusinessList = async () => {
+            setIsLoading(true);
+            try {
+                const querySnapshot = await getDocs(query(collection(db, "guide-to-hell-business-list")));
+                const businessListData = querySnapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    name: doc.data().name,
+                    description: doc.data().description,
+                    location: doc.data().location,
+                }));
+                setBusinessList(businessListData);
+                setIsLoading(false);
+            } catch (error) {
+                console.error("Error fetching schedule:", error);
+                setIsLoading(false);
+            }
+        };
+        fetchBusinessList();
+    }, []);
+
+    function handleMarkerPress({ marker }: any) {
+        setRegion(marker.location);
+    }
+
+    return (
+        <View style={styles.container}>
+            <PageHeading text="Guide to Hell" />
+            <MapView
+                ref={mapRef}
+                style={styles.map}
+                initialRegion={initialRegion}
+                region={region}
+                mapType="mutedStandard">
+                {businessList.map((marker) => {
+                    return (
+                        <Marker
+                            key={marker.name}
+                            coordinate={{ latitude: marker.location.latitude, longitude: marker.location.longitude }}
+                            title={marker.name}
+                            description={marker.description}
+                            onSelect={() => handleMarkerPress(marker)}
+                        />
+                    );
+                })}
+            </MapView>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    minWidth: "100%",
-    height: 400,
-  },
-  map: {
-    width: "100%",
-    height: "100%",
-    // minHeight: "100%",
-  },
+    container: {
+        minWidth: "100%",
+        height: 400,
+    },
+    map: {
+        width: "100%",
+        height: "100%",
+        // minHeight: "100%",
+    },
 });
